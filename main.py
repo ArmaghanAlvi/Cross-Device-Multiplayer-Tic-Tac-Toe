202f5acb61ebfe0c6314ddbe6c32cb767bfdcdf1
ArmaghanAlvi <armaghanalvi07@gmail.com>
2026-02-23 15:37:19 -0500

    Added apply_move ad check_if_won Now need to make print_board function

--- main.py
+++ main.py
@@ -58,6 +58,57 @@ class TicTacToe:
 
         client.close() #closes socket
 
+    #Function for applying moves to the board
+    def apply_move(self, move, player):
+        if self.gameOver:
+            return
+        
+        self.counter += 1
+        self.board[int(move[0])][int(move[1])] = player
+        self.print_board()
+        
+        if self.check_if_won():
+            if self.winner == self.you:
+                print("You win!")
+                exit()
+            elif self.winner == self.opponent:
+                print("You lose!")
+                exit()
+        else:
+            if self.counter == 9:
+                print("It's a tie!")
+                exit()
+
+    def check_valid_move(self, move):
+        return self.board[int(move[0])][int(move[1])] == " "
+    
+    def check_if_won(self):
+        #Checking for row wins
+        for row in range(3):
+            if self.board[row][0] == self.board[row][1] == self.board[row][2] != " ": 
+                self.winner = self.board[row][0]
+                self.game_over = True
+                return True
+            
+        #Checking for col wins
+        for col in range(3):
+            if self.board[0][col] == self.board[1][col] == self.board[2][col] != " ": 
+                self.winner = self.board[0][col]
+                self.game_over = True
+                return True
+
+        #Checking for diagonal wins
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] != " ":
+            self.winner = self.board[0][0]
+            self.game_over = True
+            return True
+
+        if self.board[2][0] == self.board[1][1] == self.board[0][2] != " ":
+            self.winner = self.board[2][0]
+            self.game_over = True
+            return True
+        
+        return False
 
 
     
